9abbb6b77a3e18aaadbd78169b99348412a53d8d
abdelhak-org <abdelhak.org@gmail.com>
2023-06-02 17:57:46 +0200

    creating folders

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router  } from 'react-router-dom';
+import NavBar from "./components/navbar/NavBar";
+import {} from "./components/index.jsx";
+import {} from "./pages/index"
+function App() {
+  return (
+    <Router>
+    <NavBar/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./navbar.css"
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
+
+export default NavBar
